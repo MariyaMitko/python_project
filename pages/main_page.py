9587564b48f7bac5fa09043026c4ab449d1a3629
MariyaMitko <mariyamitko@mail.ru>
2023-06-07 14:38:00 +0300

    Add should_be_login_link method

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,16 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
+
 class MainPage(BasePage):
+
+    def __init__(self, browser, url, timeout=10):
+        super().__init__(browser, url, timeout)
+        self.login_button = (By.CSS_SELECTOR, "#login_link")
+
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*self.login_button)
         login_link.click()
+
+    def should_be_login_link(self):
+        return self.is_element_present(*self.login_button)
