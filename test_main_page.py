84f0c755339463b542bc9847c963a6ac196a3d49
MariyaMitko <mariyamitko@mail.ru>
2023-06-13 12:57:47 +0300

    Update project structure

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 import time
 
-from ..pages.main_page import MainPage
-from ..pages.login_page import LoginPage
-from ..pages.basket_page import BasketPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 import time
 import pytest
-from ..pages.product_page import ProductPage
-from ..pages.login_page import LoginPage
-from ..pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
